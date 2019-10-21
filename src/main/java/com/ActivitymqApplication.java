9348002b2fa32209d2da55ca647550aa409d6c03
sky5cai <1147861255@qq.com>
2019-10-21 09:42:33 +0800

    Initial commit

---
+++ src/main/java/com/ActivitymqApplication.java
@@ -0,0 +1,18 @@
+package com;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ActivitymqApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitymqApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/com/activitymq/ActivitymqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitymq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitymqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
